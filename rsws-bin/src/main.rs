bad451cb097e1e620498f21650b7f72d159ace88
Anarchism-Noob <105835720+Anarchism-Noob@users.noreply.github.com>
2024-08-14 15:09:02 +0800

    first edit commit

--- rsws-bin/src/main.rs
+++ rsws-bin/src/main.rs
@@ -1,3 +1,7 @@
+use idgenerator::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut options =  IdGeneratorOptions::new().worker_id(1).worker_id_bit_len(6);
+    let _ = IdInstance::init(options);
+    let id = IdInstance::next_id();
 }
