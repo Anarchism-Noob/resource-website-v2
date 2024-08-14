8bc082611462889505e2f68725d7d30877b41f30
Anarchism-Noob <105835720+Anarchism-Noob@users.noreply.github.com>
2024-08-14 19:09:32 +0800

    touch init.rs

--- rsws_bin/src/init..rs
+++ rsws_bin/src/init..rs
@@ -2,7 +2,7 @@
  * @Author: Anarchism-Noob 105835720+Anarchism-Noob@users.noreply.github.com
  * @Date: 2024-08-14 17:53:55
  * @LastEditors: Anarchism-Noob 105835720+Anarchism-Noob@users.noreply.github.com
- * @LastEditTime: 2024-08-14 17:59:25
+ * @LastEditTime: 2024-08-14 18:02:09
  * @FilePath: /resource-website-v2/rsws-bin/src/init..rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,4 +15,5 @@ use salvo::{
     Router,
     cors::Cors, http::Method, prelude::*,
 }
-use rsws-core::
+use rsws_core::middleware::*;
+use rsws_system::get_system_router;
